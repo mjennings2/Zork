6f2b25ab6b5b0250843ac1f18d9fa2d3ce367b7f
jacksonlenz <jlenz@bayviewglen.ca>
2015-05-27 14:10:50 -0400

    Dance Harmstring Committed and Pushed

---
+++ Zork/src/com/bayviewglen/maingame/DanceHarmstring.java
@@ -0,0 +1,10 @@
+package com.bayviewglen.maingame;
+
+public class DanceHarmstring extends NPC {
+
+	public DanceHarmstring(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
