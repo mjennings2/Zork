d11eda3d529217004a4a7ebe656ef7ee9e0c40d6
jacksonlenz <jlenz@bayviewglen.ca>
2015-05-27 13:40:43 -0400

    Armory Guard Pushed for Mathew

--- Zork/src/com/bayviewglen/maingame/ArmoryGuard.java
+++ Zork/src/com/bayviewglen/maingame/ArmoryGuard.java
@@ -1,72 +1,68 @@
 package com.bayviewglen.maingame;
 
-public class ArmoryGuard extends NPC {
+public class ArmoryGuard extends NPC{
 
 	public ArmoryGuard(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
 
-	public void TalkTo(Display x, Boolean isTerrorist) {
-		boolean[] questions = { false, false, false };
-		boolean loop = true;
-		displayTextMilitaryStyle(x, "Receptionist:tu say?\n");
-		if (isTerrorist) {
-			while (loop) {
-				if (!questions[0]) {
-					displayTextMilitaryStyle(x, "1 Are you a terrorest?\n");
-				}
-				if (!questions[1]) {
-					displayTextMilitaryStyle(x,
-							"2 What are you planning to do in San Francisco?\n");
-				}
-				if (!questions[2]) {
-					displayTextMilitaryStyle(x, "3 How are the kids.\n");
-				}
-				displayTextMilitaryStyle(x, "4 Goodbye\n");
-				int input = waitForProperInput(x, "1 2 3 4");
-				if (input == 1) {
-					displayTextMilitaryStyle(x,
-							"\"EXCUUUUUUUUUUUUUUUSE ME? I AM NO TRAITOR!\"\n");
-					questions[0] = true;
-				} else if (input == 2) {
-					displayTextMilitaryStyle(x, "\"Allahu Akbar\"\n");
-					questions[1] = true;
-				} else if (input == 3) {
-					displayTextMilitaryStyle(x,
-							"\"I don't have, nor do I need kids.\"\n");
-					questions[2] = true;
-				} else {
-					loop = false;
-				}
+    public void TalkTo(Display x, Boolean isTerrorist) {
+        boolean[] questions = {
+            false, false, false
+        };
+        boolean loop = true;
+        displayTextMilitaryStyle(x, "Armory Guard: Hello, Captain! /n");
+        if (isTerrorist) {
+            while (loop) {
+                if (!questions[0]) {
+                    displayTextMilitaryStyle(x, "1 Anything to report?\n");
+                }
+                if (!questions[1]) {
+                    displayTextMilitaryStyle(x, "2 Your shoes are untied, soldier!\n");
+                }
+                if (!questions[2]) {
+                    displayTextMilitaryStyle(x, "3 Do you have any extra weapons I could use?\n");
+                }
+                displayTextMilitaryStyle(x, "4 Goodbye\n");
+                int input = waitForProperInput(x, "1 2 3 4");
+                if (input == 1) {
+                    displayTextMilitaryStyle(x, "\"Nothing here, Captain.\"\n");
+                    questions[0] = true;
+                } else if (input == 2) {
+                    displayTextMilitaryStyle(x, "\"We're buddies, eh? No need for all the formality, bro. *bends down to tie shoes*\"\n");
+                    questions[1] = true;
+                } else if (input == 3) {
+                    displayTextMilitaryStyle(x, "\"You're packing a pretty mean .45 there, cap! I don't think you need anything heavier, haha.\"\n");
+                    questions[2] = true;
+                } else {
+                    loop = false;
+                }
 
-			}
-		} else {
-			while (loop) {
-				if (!questions[0]) {
-					displayTextMilitaryStyle(x, "1 Hey How are you?\n");
-					if (!questions[1]) {
-						displayTextMilitaryStyle(x,
-								"2 What are you planning to do in San Fransisco?\n");
-					}
-					if (questions[2]) {
-						displayTextMilitaryStyle(x, "3 How are the kids.\n");
-					}
-					displayTextMilitaryStyle(x, "4 Goodbye\n");
-					int input = waitForProperInput(x, "1 2 3 4");
-					if (input == 1) {
-						displayTextMilitaryStyle(x,
-								"\"Good thanks for asking\"\n");
-						questions[0] = true;
-					} else if (input == 2) {
-						displayTextMilitaryStyle(x,
-								"\"Go see the golden gate bridge.\"\n");
-						questions[1] = true;
-					} else if (input == 3) {
-						displayTextMilitaryStyle(x, "\"They are good\"\n");
-						questions[2] = true;
-					} else {
-						loop = false;
+            }
+        } else {
+            while (loop) {
+                if (!questions[0]) {
+                    displayTextMilitaryStyle(x, "1 Anything to report?\n");
+                    if (!questions[1]) {
+                        displayTextMilitaryStyle(x, "2 Your shoes are untied, soldier!\n");
+                    }
+                    if (questions[2]) {
+                        displayTextMilitaryStyle(x, "3 Do you have any extra weapons for me?\n");
+                    }
+                    displayTextMilitaryStyle(x, "4 Goodbye\n");
+                    int input = waitForProperInput(x, "1 2 3 4");
+                    if (input == 1) {
+                        displayTextMilitaryStyle(x, "\"Sir! All clear, sir!\"\n");
+                        questions[0] = true;
+                    } else if (input == 2) {
+                        displayTextMilitaryStyle(x, "\"My apologies, sir! *bends down to tie shoes*\"\n");
+                        questions[1] = true;
+                    } else if (input == 3) {
+                        displayTextMilitaryStyle(x, "\"I'm under orders to not give anyone weapons until further notice, sir! DesLauriers' orders! \"\n");
+                        questions[2] = true;
+                    } else {
+                        loop = false;
 					}
 				}
 			}
