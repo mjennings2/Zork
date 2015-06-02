1cf08c4e544342c519cf74aa52a250a1c8660e3b
jacksonlenz <jlenz@bayviewglen.ca>
2015-06-02 09:42:34 -0400

    Commit goddamn you

--- Zork/src/com/bayviewglen/maingame/SergeantJohnson.java
+++ Zork/src/com/bayviewglen/maingame/SergeantJohnson.java
@@ -1,5 +1,78 @@
 package com.bayviewglen.maingame;
 
-public class SergeantJohnson {
+public class SergeantJohnson extends NPC {
 
+ public SergeantJohnson(String name) {
+  super(name);
+
+ }
+
+ public void TalkTo(Display x, Boolean isTerrorist) {
+  boolean[] questions = { false, false, false };
+  boolean loop = true;
+  displayTextMilitaryStyle(x,
+    "*You see feet from under the shower stall*\n");
+  if (isTerrorist) {
+   while (loop) {
+    if (!questions[0]) {
+     displayTextMilitaryStyle(x, "1 Who's in there?\n");
+    }
+    if (!questions[1]) {
+     displayTextMilitaryStyle(x,
+       "2 You'd better get out, we are almost to San Fran!\n");
+    }
+    if (!questions[2]) {
+     displayTextMilitaryStyle(x,
+       "3 Why is there a trail of blood leading to the shower?\n");
+    }
+    displayTextMilitaryStyle(x, "4 Goodbye\n");
+    int input = waitForProperInput(x, "1 2 3 4");
+    if (input == 1) {
+     displayTextMilitaryStyle(x,
+       "\"Sergeant Johnson, sir!\"\n");
+     questions[0] = true;
+    } else if (input == 2) {
+     displayTextMilitaryStyle(x, "\"No can do! I have to get clean.\"\n");
+     questions[1] = true;
+    } else if (input == 3) {
+     displayTextMilitaryStyle(x,
+       "\"Just ignore that...\"\n");
+     questions[2] = true;
+    } else {
+     loop = false;
+    }
+
+   }
+  } else {
+   while (loop) {
+    if (!questions[0]) {
+     displayTextMilitaryStyle(x, "1 Who's in there?\n");
+     if (!questions[1]) {
+      displayTextMilitaryStyle(x,
+        "2 You'd better get out, we are almost to San Fran!\n");
+     }
+     if (questions[2]) {
+      displayTextMilitaryStyle(x,
+        "3 Why is there a trail of blood leading to the shower?\n");
+     }
+     displayTextMilitaryStyle(x, "4 Goodbye\n");
+     int input = waitForProperInput(x, "1 2 3 4");
+     if (input == 1) {
+      displayTextMilitaryStyle(x,
+        "\"Sergeant Johnson, sir!\"\n");
+      questions[0] = true;
+     } else if (input == 2) {
+      displayTextMilitaryStyle(x,
+        "\"Yes sir. Right away sir!\"\n");
+      questions[1] = true;
+     } else if (input == 3) {
+      displayTextMilitaryStyle(x, "\"I am prone to nosebleeds, sir!\"\n");
+      questions[2] = true;
+     } else {
+      loop = false;
+     }
+    }
+   }
+  }
+ }
 }
--- Zork/src/com/bayviewglen/maingame/SergeantMajorSmith.java
+++ Zork/src/com/bayviewglen/maingame/SergeantMajorSmith.java
@@ -1,5 +1,75 @@
 package com.bayviewglen.maingame;
 
-public class SergeantMajorSmith {
+public class SergeantMajorSmith extends NPC {
 
+ public SergeantMajorSmith(String name) {
+  super(name);
+
+ }
+
+ public void TalkTo(Display x, Boolean isTerrorist) {
+  boolean[] questions = { false, false, false };
+  boolean loop = true;
+  displayTextMilitaryStyle(x,
+    "*Smith is sitting on a chair in the Barracks*\n");
+  if (isTerrorist) {
+   while (loop) {
+    if (!questions[0]) {
+     displayTextMilitaryStyle(x, "1 When is your next shift?\n");
+    }
+    if (!questions[1]) {
+     displayTextMilitaryStyle(x, "2 Is anyone missing?\n");
+    }
+    if (!questions[2]) {
+     displayTextMilitaryStyle(x, "3 Seen anything suspicious?\n");
+    }
+    displayTextMilitaryStyle(x, "4 Goodbye\n");
+    int input = waitForProperInput(x, "1 2 3 4");
+    if (input == 1) {
+     displayTextMilitaryStyle(x,
+       "\"Whenever we get to San Fran!\"\n");
+     questions[0] = true;
+    } else if (input == 2) {
+     displayTextMilitaryStyle(x, "\"Nope.\"\n");
+     questions[1] = true;
+    } else if (input == 3) {
+     displayTextMilitaryStyle(x,
+       "\"Suspicious? On this train? Pft. Yeah right.\"\n");
+     questions[2] = true;
+    } else {
+     loop = false;
+    }
+
+   }
+  } else {
+   while (loop) {
+    if (!questions[0]) {
+     displayTextMilitaryStyle(x, "1 When is your next shift?\n");
+     if (!questions[1]) {
+      displayTextMilitaryStyle(x, "2 Is anyone missing?\n");
+     }
+     if (questions[2]) {
+      displayTextMilitaryStyle(x,
+        "3 Seen anything suspicious?\n");
+     }
+     displayTextMilitaryStyle(x, "4 Goodbye\n");
+     int input = waitForProperInput(x, "1 2 3 4");
+     if (input == 1) {
+      displayTextMilitaryStyle(x,
+        "\"At 1300.\"\n");
+      questions[0] = true;
+     } else if (input == 2) {
+      displayTextMilitaryStyle(x,
+        "\"Yeah, the Sergeant hasn't been around.\"\n");
+      questions[1] = true;
+     } else if (input == 3) {
+      displayTextMilitaryStyle(x, "\"No Sir!\"\n");
+      questions[2] = true;
+     } else {
+      loop = false;
+     }
+    }
+   }
+  }
+ }
 }
